99a1b9f76acc123c0dd574bd2b843dfe22b5da52
Talkee <aglicheev.ao@gmail.com>
2022-01-04 13:20:14 +1000

    Add id to SectionResource

--- app/Http/Resources/SectionResource.php
+++ app/Http/Resources/SectionResource.php
@@ -15,6 +15,7 @@ class SectionResource extends JsonResource
     public function toArray($request) : array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'text' => $this->text,
             'file' => $this->file,
