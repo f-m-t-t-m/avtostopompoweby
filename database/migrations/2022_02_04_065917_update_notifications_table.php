3600266c774362471936e9537545c00a0b86b7fb
erofeevAA <erofeev.aa@students.dvfu.ru>
2022-02-04 23:35:10 +1000

    add update notification

---
+++ database/migrations/2022_02_04_065917_update_notifications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->foreignId("subject_id")->constrained()->cascadeOnDelete();
+            $table->foreignId("section_id")->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->dropColumn("subject_id");
+            $table->dropColumn("section_id");
+        });
+    }
+}
